b87267d9a5b99eedecc599a2ff9921a8c9555de1
mounishvatti <mounishvatti2002@gmail.com>
2024-09-08 22:46:51 +0530

    added encryption

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cors = require('cors'); // Import cors middleware
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(process.env.CRYPTR_SECRET || 'ALL_I_CARE_ABOUT_IS_CRYPTR_SECRET');
 const app = express();
 const JWT_SECRET = process.env.JWT_SECRET || 'ALL_I_CARE_ABOUT_IS_JWT_SECRET';
 
@@ -24,13 +26,14 @@ app.post('/register', (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
+  const encryptedPassword = cryptr.encrypt(password);
   const confirmPassword = req.body.confirmPassword;
 
   if (name && email && password && confirmPassword) {
     if (password !== confirmPassword) {
       return res.status(400).send('Passwords do not match');
     }
-    const user = { name, email, password };
+    const user = { name, email, encryptedPassword };
     users.push(user);
     saveUsers();
     res.send(users);
@@ -43,10 +46,10 @@ app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = users.find((user) => user.email === email && user.password === password);
+  const user = users.find((user) => user.email === email && cryptr.decrypt(user.encryptedPassword) === password);
 
   if (user) {
-    const token = jwt.sign({ email: user.email }, JWT_SECRET);
+    const token = jwt.sign({ email: user.email }, JWT_SECRET, { expiresIn: '2 days' });
     res.send({ token });
   } else {
     res.status(401).send('Invalid Credentials');
